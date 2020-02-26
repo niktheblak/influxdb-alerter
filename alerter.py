2fb59a600d00c19ee77adc04448cb6c3b14fe02e
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2020-02-26 11:43:58 +0200

    Configuration for email

--- alerter.py
+++ alerter.py
@@ -10,32 +10,37 @@ from dateutil import parser
 from influxdb import InfluxDBClient
 
 
-def age(point):
+def point_ts(point):
     t = point['time']
-    ts = parser.isoparse(t).replace(tzinfo=pytz.utc)
+    return parser.isoparse(t).replace(tzinfo=pytz.utc)
+
+
+def age(ts):
     now = datetime.utcnow().replace(tzinfo=pytz.utc)
-    diff = now - ts
-    return ts, diff
+    return now - ts
 
 
 def send_email(cfg, last_measurement_ts):
-    with smtplib.SMTP(cfg['smtp']['host'].get(str), cfg['smtp']['port'].get(465)) as s:
+    email_cfg = cfg['email']
+    with smtplib.SMTP(email_cfg['host'].get(str), email_cfg['port'].get(465)) as s:
         msg = EmailMessage()
         msg.set_charset('UTF-8')
-        msg.set_content(f"There has been no new measurements in InfluxDB since {last_measurement_ts.ctime()}.")
+        msg.set_content(
+            f"There has been no new measurements in InfluxDB since {last_measurement_ts.ctime()}.")
         msg['Subject'] = "No new InfluxDB measurements"
-        msg['From'] = cfg['smtp']['from'].get(str)
-        msg['To'] = cfg['smtp']['to'].get(str)
+        msg['From'] = email_cfg['from'].get(str)
+        msg['To'] = email_cfg['to'].get(str)
         s.starttls()
-        s.login(cfg['smtp']['username'].get(str),
-                cfg['smtp']['password'].get(str))
+        s.login(email_cfg['username'].get(str),
+                email_cfg['password'].get(str))
         s.send_message(msg)
 
 
 logging.basicConfig(level=logging.INFO)
 config = confuse.Configuration('influxdb-alerter', __name__)
 max_age = timedelta(seconds=config['max_age'].get(600))
-email_sent_file = config['email_sent_file'].as_filename()
+email_enabled = config['email']['enabled'].get(True)
+email_sent_file = config['email']['sent_file'].as_filename()
 client = InfluxDBClient(config['influxdb']['host'].get('localhost'),
                         config['influxdb']['port'].get(8086),
                         config['influxdb']['username'].get(str),
@@ -50,17 +55,17 @@ points = result.get_points(measurement=f"{table}")
 p = next(points)
 points.close()
 client.close()
-(ts, diff) = age(p)
+ts = point_ts(p)
+diff = age(ts)
 local_ts = ts.astimezone()
-secs = diff.total_seconds()
 logging.info(
     "Last measurement was %s seconds ago at %s",
-    secs,
+    diff.total_seconds(),
     local_ts.ctime())
 path = Path(email_sent_file)
 if diff > max_age:
     logging.warning("No new measurements since %s", local_ts.ctime())
-    if not path.exists():
+    if email_enabled and not path.exists():
         send_email(config, local_ts)
         path.touch()
 else:
